6400257041a321a570eafadb28c39cd5d6bdd416
chris2rich <cz07business@gmail.com>
2024-09-08 20:24:29 +0100

    initialized

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "net/http"
+  "io/ioutil"
+  "log"
+)
+
+func main() {
+  content, err_initio := ioutil.ReadFile("index.html")
+  if(err_initio != nil){
+    log.Fatal("Error: Server failed to initialize - html was not read")
+    return
+  }
+
+  http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/html")
+    w.Write(content)
+    return
+  })
+
+  log.Fatal(http.ListenAndServe(":80", nil))
+  return
+}
