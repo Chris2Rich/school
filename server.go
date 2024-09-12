cae042665059ca92eb5d7a09701d3b7922cbb04b
Chris Zac <cz07business@gmail.com>
2024-09-12 08:53:15 +0000

    Update 2 files
    - /todo_list.csv
    - /server.go

--- server.go
+++ server.go
@@ -3,28 +3,45 @@ package main
 import (
   "net/http"
   "io/ioutil"
+  "os"
+  "encoding/csv"
   "log"
 )
 
 func main() {
-  content, err_initio := ioutil.ReadFile("index.html")
-  if(err_initio != nil){
-    log.Fatal("Error: Server failed to initialize - html was not read")
+  //Initialize error table
+  err_t := [2]error{}
+
+  //Loads all content from files when sever initializes
+  static_html_content, tmp := ioutil.ReadFile("index.html")
+  err_t[0] = tmp
+  todo_list_file, tmp = os.Open("todo_list.csv")
+  err_t[1] = tmp
+
+  //If any errors in loading critical data, program crashes
+  if(err_static_initio != nil || err_data_initio != nil){
+    log.Panic("Error: Server failed to initialize", err_table)
     return
   }
 
+  //All files opened are closed when program terminates
+  defer todo_list_file.Close()
+
+  //Generic response for http
   http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "text/html")
-    w.Write(content)
+    w.Write(static_html_content)
     return
   })
  
+  //Routes 
   http.HandleFunc("/fetch/todo", func(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "text/json")
-    w.Write(content)
+    w.Write(todo_list_content)
     return
   })
 
-  log.Fatal(http.ListenAndServe(":80", nil))
+  //http server listens on port 80 and program crashes if any errors
+  log.Panic(http.ListenAndServe(":80", nil))
   return
 }
