483da208d41aa960b2c98b01d61568d75b9fac06
Chris Zac <cz07business@gmail.com>
2024-11-13 17:13:24 +0000

    !?

--- server.go
+++ server.go
@@ -1,32 +1,44 @@
 package main
 
 import (
-  "net/http"
-  "io"
-  "io/ioutil"
-  "os"
-  "bufio"
-  "log"
-  "errors"
-  "bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
     w.Header().Set("Content-Type", "text/html")
-    u, err := url.Parse(req.URL.path)
+    u, err := url.Parse(req.URL.Path)
     if err == nil {
-      html, read_err := ioutil.ReadFile("index.html")
+      if strings.HasPrefix(u.String(), "key") {
+        w.Write([]byte("Unauthorized access."))
+        return
+      }
+      if u.String() == "/" {
+        html, read_err := ioutil.ReadFile("index.html")
+        if read_err != nil {
+          w.Write([]byte("File failed to open."))
+        } else {
+          w.Write(html)
+        }
+        return
+      }
+      html, read_err := ioutil.ReadFile(u.String())
       if read_err != nil {
-        w.Write("File failed to open.")
-      } else
+        w.Write([]byte("File failed to open."))
+      } else {
+        w.Write(html)
+      }
     } else {
-      w.Write("Server could not read url.")
+      w.Write([]byte("Server could not read url."))
     }
     
     return
   })
 
-  go log.Panic(http.ListenAndServeTLS(":443", "crt.crt", "key.key", nil)) 
+  go log.Panic(http.ListenAndServeTLS(":443", "secret/crt.crt", "secret/key.key", nil)) 
   return
 }
